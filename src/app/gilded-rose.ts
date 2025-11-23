7f12850932b95e7e2df52a3c82fd83d6240e79da
Jean-Karim Bockstael <jkb@jkbockstael.be>
2025-11-23 20:20:52 +0100

    refactor: refactor to a strategy pattern
    Lifting up the conditionals helped a lot with readability, but the
    `GildedRose` class still handled both inventory keeping and item quality
    updates.
    
    This refactors to a Strategy pattern, to separate concerns and
    communicate more clearly the kind of problem the program solves.

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -1,33 +1,25 @@
-enum ItemType {
-  AGING,
-  LEGENDARY,
-  PASS,
-  CONJURED,
-  REGULAR
+interface ItemUpdateStrategy {
+  updateItem(item: Item): Item;
 }
 
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
-
-  constructor(name: string, sellIn: number, quality: number) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
-
-export class GildedRose {
-  items: Array<Item>;
-
-  constructor(items = [] as Array<Item>) {
-    this.items = items;
-  }
-
-  updateAgingItem(item: Item): Item {
+class RegularItemStrategy implements ItemUpdateStrategy {
+  updateItem(item: Item): Item {
     item.sellIn -= 1;
-    item.quality +=
+    item.quality -=
+      (item.sellIn >= 0)
+      ? 1
+      : 2;
+    if (item.quality < 0) {
+      item.quality = 0;
+    }
+    return item;
+  }
+}
+
+class AgingItemUpdateStrategy implements ItemUpdateStrategy {
+  updateItem(item: Item): Item {
+    item.sellIn -= 1;
+    item.quality += 
       (item.sellIn < 0)
       ? 2
       : 1;
@@ -36,13 +28,17 @@ export class GildedRose {
     }
     return item;
   }
+}
 
-  updateLegendaryItem(item: Item): Item {
+class LegendaryItemUpdateStrategy implements ItemUpdateStrategy {
+  updateItem(item: Item): Item {
     // no-op
     return item;
   }
+}
 
-  updateBackstagePassItem(item: Item): Item {
+class PassItemUpdateStrategy implements ItemUpdateStrategy {
+  updateItem(item: Item): Item {
     item.sellIn -= 1;
     item.quality += 1;
     if (item.sellIn < 10) {
@@ -59,8 +55,10 @@ export class GildedRose {
     }
     return item;
   }
+}
 
-  updateConjuredItem(item: Item): Item {
+class ConjuredItemUpdateStrategy implements ItemUpdateStrategy {
+  updateItem(item: Item): Item {
     item.sellIn -= 1;
     item.quality -=
       (item.sellIn >= 0)
@@ -71,20 +69,29 @@ export class GildedRose {
     }
     return item;
   }
+}
 
-  updateRegularItem(item: Item): Item {
-    item.sellIn -= 1;
-    item.quality -=
-      (item.sellIn >= 0)
-      ? 1
-      : 2;
-    if (item.quality < 0) {
-      item.quality = 0;
-    }
-    return item;
-  }
+enum ItemType {
+  AGING,
+  LEGENDARY,
+  PASS,
+  CONJURED,
+  REGULAR
+}
 
-  itemType(item: Item): ItemType {
+class ItemUpdater {
+  strategies: Map<ItemType, ItemUpdateStrategy>;
+
+  constructor() {
+    this.strategies = new Map<ItemType, ItemUpdateStrategy>();
+    this.strategies.set(ItemType.AGING, new AgingItemUpdateStrategy());
+    this.strategies.set(ItemType.LEGENDARY, new LegendaryItemUpdateStrategy());
+    this.strategies.set(ItemType.PASS, new PassItemUpdateStrategy());
+    this.strategies.set(ItemType.CONJURED, new ConjuredItemUpdateStrategy());
+    this.strategies.set(ItemType.REGULAR, new RegularItemStrategy());
+  };
+
+  private itemType(item: Item): ItemType {
     if (item.name.startsWith("Conjured")) {
       return ItemType.CONJURED;
     }
@@ -100,23 +107,35 @@ export class GildedRose {
     }
   }
 
-  updateItemQuality(item: Item): Item {
-    switch (this.itemType(item)) {
-      case ItemType.AGING:
-        return this.updateAgingItem(item);
-      case ItemType.LEGENDARY:
-        return this.updateLegendaryItem(item);
-      case ItemType.PASS:
-        return this.updateBackstagePassItem(item);
-      case ItemType.CONJURED:
-        return this.updateConjuredItem(item);
-      case ItemType.REGULAR:
-        return this.updateRegularItem(item);
-    }
+  updateItem(item: Item): Item {
+    // "as ItemUpdateStrategy" because the map is in fact exhaustive
+    return (this.strategies.get(this.itemType(item)) as ItemUpdateStrategy).updateItem(item);
+  }
+}
+
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
+
+  constructor(name: string, sellIn: number, quality: number) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
+
+export class GildedRose {
+  items: Array<Item>;
+  updater: ItemUpdater;
+
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+    this.updater = new ItemUpdater();
   }
 
   updateQuality() {
-    this.items = this.items.map(this.updateItemQuality, this);
+    this.items.forEach(item => this.updater.updateItem(item))
     return this.items;
   }
 }
